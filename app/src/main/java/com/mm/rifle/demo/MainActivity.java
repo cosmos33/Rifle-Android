f94d8440489d22c8246bbd077290d2ccfaef97cf
wangwang <chen.wangwang@immomo.com>
2019-04-02 10:40:06 +0800

    修改：添加read_phone权限

--- app/src/main/java/com/mm/rifle/demo/MainActivity.java
+++ app/src/main/java/com/mm/rifle/demo/MainActivity.java
@@ -1,7 +1,14 @@
 package com.mm.rifle.demo;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Process;
 import android.view.View;
 import android.widget.Button;
 
@@ -9,9 +16,34 @@ import com.mm.rifle.Rifle;
 
 public class MainActivity extends Activity {
 
+    private static final int READ_PHONE_STATE = 100;
+
+    public static int checkSelfPermission(Context context, String permission) {
+        if (permission == null) {
+            throw new IllegalArgumentException("permission is null");
+        } else {
+            return context.checkPermission(permission, Process.myPid(), Process.myUid());
+        }
+    }
+
+    public static void requestPermissions(final Activity activity, final String[] permissions, final int requestCode) {
+            if (Build.VERSION.SDK_INT >= 23) {
+                activity.requestPermissions(permissions, requestCode);
+            }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE},
+                    READ_PHONE_STATE);
+        }
+
         setContentView(R.layout.activity_main);
 
         Button javaCrash = findViewById(R.id.java_crash);
